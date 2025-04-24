5145ba3d509df3b342511f454ba2bf22c689fe9d
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-04-23 23:22:27 -0400

    Add brute force win condition check

--- scripts.js
+++ scripts.js
@@ -15,11 +15,90 @@ const gameBoard = (function () {
     return gameBoard;
   };
 
-  return { getBoard };
+  const playSquare = function (row, column, mark) {
+    gameBoard[row][column] = mark;
+    console.log(gameBoard);
+    console.log(this.checkWin());
+  };
+
+  const checkWin = function () {
+    // First Row
+    if (
+      gameBoard[0][0] === gameBoard[0][1] &&
+      gameBoard[0][0] === gameBoard[0][2]
+    ) {
+      return true;
+    }
+    // Second Row
+    else if (
+      gameBoard[1][0] === gameBoard[1][1] &&
+      gameBoard[1][0] === gameBoard[1][2]
+    ) {
+      return true;
+    }
+    // Third Row
+    else if (
+      gameBoard[2][0] === gameBoard[2][1] &&
+      gameBoard[2][0] === gameBoard[2][2]
+    ) {
+      return true;
+    }
+    // First Column
+    else if (
+      gameBoard[0][0] === gameBoard[1][0] &&
+      gameBoard[0][0] === gameBoard[2][0]
+    ) {
+      return true;
+    }
+    // Second Column
+    else if (
+      gameBoard[0][1] === gameBoard[1][1] &&
+      gameBoard[0][1] === gameBoard[2][1]
+    ) {
+      return true;
+    }
+    // Third Column
+    else if (
+      gameBoard[0][2] === gameBoard[1][2] &&
+      gameBoard[0][2] === gameBoard[2][2]
+    ) {
+      return true;
+    }
+    // Top-Left -> Bottom-Right Diagonal
+    else if (
+      gameBoard[0][0] === gameBoard[1][1] &&
+      gameBoard[0][0] === gameBoard[2][2]
+    ) {
+      return true;
+    }
+    // Bottom-Left -> Top-Right Diagonal
+    else if (
+      gameBoard[2][0] === gameBoard[1][1] &&
+      gameBoard[2][0] === gameBoard[0][2]
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return { getBoard, playSquare, checkWin };
 })();
 
-function Player() {}
+// function Player(name, mark) {
+//   return { name, mark };
+// }
 
-function Game() {}
+// function Game() {
+//   const player1 = Player("Player1", "X");
+//   const player2 = Player("Player2", "O");
+
+//   let turn = 1;
+
+//   const playTurn = function () {};
+// }
 
 gameBoard.getBoard();
+gameBoard.playSquare(2, 0, "X");
+gameBoard.playSquare(1, 1, "X");
+gameBoard.playSquare(0, 2, "X");
